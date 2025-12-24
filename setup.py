e844779ad0e2155906bb0752bdfc8ea3e8195f48
Subhasish Das <anita80subhasish@gmail.com>
2025-12-24 21:04:10 +0530

    reqrmnt done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medbotflask',
+    version='0.1',
+    author='Subhasish',
+    packages=find_packages(),
+    install_requires=[]
+)
